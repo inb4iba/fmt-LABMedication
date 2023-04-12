085ebd51c6e4aa3121659b09037bee9a0af874b7
Icaro Andrade <iba1987@gmail.com>
2023-04-12 15:38:37 -0300

    create toast alert component

---
+++ src/app/components/toast-alert/toast-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'toast-alert',
+  templateUrl: './toast-alert.component.html',
+  styleUrls: ['./toast-alert.component.css']
+})
+export class ToastAlertComponent {
+
+}
