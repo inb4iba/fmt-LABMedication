ed86443b571d07ae48a8681011afadd82ba039f5
Icaro Andrade <iba1987@gmail.com>
2023-04-12 17:04:40 -0300

    register layout

--- src/app/pages/login/register/register.component.ts
+++ src/app/pages/login/register/register.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  showing = false;
+  @Output() closeRegister = new EventEmitter<any>();
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.showing = true;
+    }, 0);
+  }
+
+  goToLogin() {
+    this.showing = false;
+    setTimeout(() => {
+      this.closeRegister.emit();
+    }, 700);
+  }
 }
