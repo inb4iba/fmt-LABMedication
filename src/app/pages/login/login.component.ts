e7822f0539492d1b284ed2d1d3ef93b8f6d207c6
Icaro Andrade <iba1987@gmail.com>
2023-04-12 16:12:02 -0300

    show alert on forgot password click

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,4 +5,14 @@ import { Component } from "@angular/core";
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  forgotPassword = false;
+
+  removePasswordAlert() {
+    this.forgotPassword = false;
+  }
+
+  showPasswordAlert() {
+    this.forgotPassword = true;
+  }
+}
