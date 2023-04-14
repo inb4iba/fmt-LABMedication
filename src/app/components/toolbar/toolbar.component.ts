77dcd62d92a70c6c5faeb25d6f0af91faafbcfee
Icaro Andrade <iba1987@gmail.com>
2023-04-14 15:18:31 -0300

    display connected username and fix visibility

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ConnectionService } from "src/app/shared/services/connection.service";
 
 @Component({
   selector: "toolbar",
   templateUrl: "./toolbar.component.html",
   styleUrls: ["./toolbar.component.css"],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   user = "Usuário Teste";
+
+  constructor(private connectionService: ConnectionService) {}
+
+  ngOnInit(): void {
+    this.user = this.connectionService.getUser();
+  }
 }
