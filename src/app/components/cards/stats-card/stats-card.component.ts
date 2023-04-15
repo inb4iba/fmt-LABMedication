152fd2bd106e36fc31f4d0d7e3e5d61b5abd3adf
Icaro <iba1987@gmail.com>
2023-04-15 14:43:04 -0300

    create home components

---
+++ src/app/components/cards/stats-card/stats-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stats-card',
+  templateUrl: './stats-card.component.html',
+  styleUrls: ['./stats-card.component.css']
+})
+export class StatsCardComponent {
+
+}
