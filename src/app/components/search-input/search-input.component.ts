044d181b61b52f35504f7343b0e6b70690d6d004
Icaro <iba1987@gmail.com>
2023-04-15 15:20:24 -0300

    basic layout for home and search-input

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'search-input',
-  templateUrl: './search-input.component.html',
-  styleUrls: ['./search-input.component.css']
+  selector: "search-input",
+  templateUrl: "./search-input.component.html",
+  styleUrls: ["./search-input.component.css", "../../app.component.css"],
 })
-export class SearchInputComponent {
-
-}
+export class SearchInputComponent {}
