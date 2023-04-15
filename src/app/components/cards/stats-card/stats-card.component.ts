8753702259534526769c69e347188fde29c75e66
Icaro <iba1987@gmail.com>
2023-04-15 15:48:11 -0300

    stats card layout

--- src/app/components/cards/stats-card/stats-card.component.ts
+++ src/app/components/cards/stats-card/stats-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'stats-card',
-  templateUrl: './stats-card.component.html',
-  styleUrls: ['./stats-card.component.css']
+  selector: "stats-card",
+  templateUrl: "./stats-card.component.html",
+  styleUrls: ["./stats-card.component.css"],
 })
 export class StatsCardComponent {
-
+  @Input() icon = "";
+  @Input() text = "";
+  @Input() value = "";
 }
