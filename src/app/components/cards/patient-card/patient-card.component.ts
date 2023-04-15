a97a78f92ba0879ae7539d3770c161bb662458e8
Icaro <iba1987@gmail.com>
2023-04-15 18:23:58 -0300

    update patient card info via var

--- src/app/components/cards/patient-card/patient-card.component.ts
+++ src/app/components/cards/patient-card/patient-card.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'patient-card',
-  templateUrl: './patient-card.component.html',
-  styleUrls: ['./patient-card.component.css']
+  selector: "patient-card",
+  templateUrl: "./patient-card.component.html",
+  styleUrls: ["./patient-card.component.css"],
 })
 export class PatientCardComponent {
-
+  @Input() name = "";
+  @Input() plan = "";
+  @Input() id = -1;
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent {
-
+  patients = [
+    {
+      id: 0,
+      name: "Fábio Lima",
+      plan: "Unimed",
+    },
+    {
+      id: 1,
+      name: "Leandro do Carmo",
+      plan: "Unimed",
+    },
+    {
+      id: 2,
+      name: "Mathias Santtiago",
+      plan: "Unimed",
+    },
+    {
+      id: 3,
+      name: "Calvino Delgado",
+      plan: "Unimed",
+    },
+    {
+      id: 4,
+      name: "Jennifer Costa",
+      plan: "Unimed",
+    },
+    {
+      id: 5,
+      name: "Valéria Soares",
+      plan: "Unimed",
+    },
+  ];
 }
