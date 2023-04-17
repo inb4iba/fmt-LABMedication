aceab4ce3e8cfa2effa7ffc3e16b2b8a087adff9
Icaro Andrade <iba1987@gmail.com>
2023-04-17 17:13:50 -0300

    update home patients from service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,43 +1,47 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import {
+  IPatient,
+  PatientsService,
+} from "src/app/shared/services/patients.service";
 
 @Component({
   selector: "home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-  patients = [
-    {
-      id: 0,
-      name: "Fábio Lima",
-      plan: "Unimed",
-    },
-    {
-      id: 1,
-      name: "Leandro do Carmo",
-      plan: "Unimed",
-    },
-    {
-      id: 2,
-      name: "Mathias Santtiago",
-      plan: "Unimed",
-    },
-    {
-      id: 3,
-      name: "Calvino Delgado",
-      plan: "Unimed",
-    },
-    {
-      id: 4,
-      name: "Jennifer Costa",
-      plan: "Unimed",
-    },
-    {
-      id: 5,
-      name: "Valéria Soares",
-      plan: "Unimed",
-    },
-  ];
+export class HomeComponent implements OnInit {
+  // patients = [
+  //   {
+  //     id: 0,
+  //     name: "Fábio Lima",
+  //     plan: "Unimed",
+  //   },
+  //   {
+  //     id: 1,
+  //     name: "Leandro do Carmo",
+  //     plan: "Unimed",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Mathias Santtiago",
+  //     plan: "Unimed",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Calvino Delgado",
+  //     plan: "Unimed",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Jennifer Costa",
+  //     plan: "Unimed",
+  //   },
+  //   {
+  //     id: 5,
+  //     name: "Valéria Soares",
+  //     plan: "Unimed",
+  //   },
+  // ];
   stats = [
     {
       cardType: "patient",
@@ -48,4 +52,11 @@ export class HomeComponent {
       value: "32",
     },
   ];
+  patients: Array<IPatient> = [];
+
+  constructor(private patientsService: PatientsService) {}
+
+  ngOnInit(): void {
+    this.patients = this.patientsService.getPatients();
+  }
 }
