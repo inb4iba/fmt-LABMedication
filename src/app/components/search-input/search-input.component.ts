9790c2bf3ed3185cb2aa0a23f205c2552d11b496
Icaro Andrade <iba1987@gmail.com>
2023-04-18 17:31:51 -0300

    search input functionality

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -1,8 +1,15 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "search-input",
   templateUrl: "./search-input.component.html",
   styleUrls: ["./search-input.component.css", "../../app.component.css"],
 })
-export class SearchInputComponent {}
+export class SearchInputComponent {
+  @Output() getInputValue = new EventEmitter<string>();
+  input = "";
+
+  passInput() {
+    this.getInputValue.emit(this.input);
+  }
+}
