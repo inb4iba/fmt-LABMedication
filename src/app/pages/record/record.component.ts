a04863b14e03e9916b4213e8a227679358e4425f
Icaro Andrade <iba1987@gmail.com>
2023-04-19 15:11:23 -0300

    page layout

--- src/app/pages/record/record.component.ts
+++ src/app/pages/record/record.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  IPatient,
+  PatientsService,
+} from "src/app/shared/services/patients.service";
 
 @Component({
-  selector: 'record',
-  templateUrl: './record.component.html',
-  styleUrls: ['./record.component.css']
+  selector: "record",
+  templateUrl: "./record.component.html",
+  styleUrls: ["./record.component.css"],
 })
-export class RecordComponent {
+export class RecordComponent implements OnInit {
+  patients: Array<IPatient>;
 
+  constructor(private patientsService: PatientsService) {
+    this.patients = patientsService.getPatients();
+  }
+
+  ngOnInit(): void {
+    this.patients = this.patientsService.getPatients();
+  }
 }
